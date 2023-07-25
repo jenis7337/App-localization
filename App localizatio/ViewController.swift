f749e21965b76e8f2ad8790e21464654020cb79f
jenis navadiya <jenisnavadiya04@gmail.com>
2023-07-25 14:49:22 +0530

    ..

--- App localizatio/ViewController.swift
+++ App localizatio/ViewController.swift
@@ -1,19 +1,47 @@
-//
-//  ViewController.swift
-//  App localizatio
-//
-//  Created by Jenish Navadiya on 24/07/23.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+ 
+    @IBOutlet weak var lb1: UILabel!
+    @IBOutlet weak var txt1: UITextField!
+    @IBOutlet weak var lan: UIPickerView!
+    var language = ["English","Hindi","Gujrati"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return language.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return language[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if language[row] == "English"{
+            changeLanguage(lang: "en")
+        }
+        else if language[row] == "Hindi" {
+            changeLanguage(lang: "hi")
+        }
+        else {
+            changeLanguage(lang: "gu-IN")
+        }
+    }
+    func changeLanguage(lang : String){
+        lb1.text = "title".localizeString(string: lang)
+        txt1.placeholder = "info".localizeString(string: lang)
+    }
+}
 
-
+extension String {
+    func localizeString(string:String) -> String {
+        let path = Bundle.main.path(forResource: string, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        return NSLocalizedString(self,tableName: nil,bundle: bundle!,value: "", comment: "")
+    }
 }
 
